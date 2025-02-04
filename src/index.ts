6aeaa68d9218d8b768de7e32c0d522d3765927f5
ny64 <me@ny64.kr>
2025-02-04 21:45:40 +0900

    school 엔드포인트 완성

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import { swagger } from '@elysiajs/swagger';
+import { logger } from '@grotto/logysia';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import school from './routes/school';
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+	.use(
+		swagger({
+			documentation: {
+				info: {
+					title: 'Slunch-V2 API',
+					description: 'API for slunch-v2',
+					version: '1.0.0',
+				},
+				servers: [
+					// {
+					// 	url: 'https://slunch-v2.ny64.kr',
+					// 	description: 'Production server',
+					// },
+					{
+						url: 'http://localhost:3000',
+						description: 'Local server',
+					},
+				],
+			},
+		})
+	)
+	.use(
+		logger({
+			logIP: true,
+			writer: {
+				write(msg: string) {
+					console.log(msg);
+				},
+			},
+		})
+	)
+
+	.use(school)
+	.listen(3000);
+
+console.log(`
+🍤 Slunch-V2 backend is running at ${app.server!.url}
+📄 Swagger documentation is available at ${app.server!.url}docs
+`);
