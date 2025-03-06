26ab51c8af764aadc672f4f10d2a9de88127ebfc
ny64 <me@ny64.kr>
2025-03-06 12:18:07 +0900

    타입 에러 수정

--- src/schema.ts
+++ src/schema.ts
@@ -6,7 +6,7 @@ const mealSchema = new db.Schema({
 	type: String,
 	origin: [{ food: String, origin: String }],
 	calorie: String,
-	nutrition: [{ type: String, amount: String }],
+	nutrition: [{ type: { type: String, required: true }, amount: { type: String, required: true } }],
 });
 
 export type MealSchema = db.InferSchemaType<typeof mealSchema>;
