fb26db9f9ca86d101fac3185c5c207ddc66e3433
ny64 <me@ny64.kr>
2025-03-08 01:20:47 +0900

    change logger

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,39 @@
 import { Elysia } from 'elysia';
 import { swagger } from '@elysiajs/swagger';
-import { logger } from '@grotto/logysia';
+import { Logestic } from 'logestic';
 
 import comcigan from './routes/comcigan';
 import neis from './routes/neis';
 import { cronjob } from './cache';
 
 const app = new Elysia()
-  .use(
-    swagger({
-      documentation: {
-        info: {
-          title: 'Slunch-V2 API',
-          description: 'API for slunch-v2',
-          version: '1.0.0',
-        },
-        servers: [
-          {
-            url: 'https://slunch-v2.ny64.kr',
-            description: 'Production server',
-          },
-          {
-            url: 'http://localhost:3000',
-            description: 'Local server',
-          },
-        ],
-      },
-    })
-  )
-  .use(
-    logger({
-      logIP: true,
-      writer: {
-        write(msg: string) {
-          console.log(msg);
-        },
-      },
-    })
-  )
+	.use(
+		swagger({
+			documentation: {
+				info: {
+					title: 'Slunch-V2 API',
+					description: 'API for slunch-v2',
+					version: '1.0.0',
+				},
+				servers: [
+					{
+						url: 'https://slunch-v2.ny64.kr',
+						description: 'Production server',
+					},
+					{
+						url: 'http://localhost:3000',
+						description: 'Local server',
+					},
+				],
+			},
+		})
+	)
+	.use(Logestic.preset('fancy'))
 
-  .use(cronjob)
-  .use(comcigan)
-  .use(neis)
-  .listen(process.env.PORT ?? 3000);
+	.use(cronjob)
+	.use(comcigan)
+	.use(neis)
+	.listen(process.env.PORT ?? 3000);
 
 console.log(`
 🍤 Slunch-V2 backend is running at ${app.server!.url}
