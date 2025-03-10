c4cd0aeb9ee56d1df3ff53b2b0b27f3367cc61f9
misilelab <misileminecord@gmail.com>
2025-03-10 10:35:26 +0900

    tab to spaces
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -8,34 +8,34 @@ import notifications from './routes/notifications';
 import { cronjob } from './libraries/cache';
 
 const app = new Elysia()
-	.use(
-		swagger({
-			documentation: {
-				info: {
-					title: 'Slunch-V2 API',
-					description: 'API for slunch-v2',
-					version: '1.0.0',
-				},
-				servers: [
-					{
-						url: 'https://slunch-v2.ny64.kr',
-						description: 'Production server',
-					},
-					{
-						url: 'http://localhost:3000',
-						description: 'Local server',
-					},
-				],
-			},
-		})
-	)
-	.use(Logestic.preset('fancy'))
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: 'Slunch-V2 API',
+          description: 'API for slunch-v2',
+          version: '1.0.0',
+        },
+        servers: [
+          {
+            url: 'https://slunch-v2.ny64.kr',
+            description: 'Production server',
+          },
+          {
+            url: 'http://localhost:3000',
+            description: 'Local server',
+          },
+        ],
+      },
+    })
+  )
+  .use(Logestic.preset('fancy'))
 
-	.use(cronjob)
-	.use(comcigan)
-	.use(neis)
-	.use(notifications)
-	.listen(process.env.PORT ?? 3000);
+  .use(cronjob)
+  .use(comcigan)
+  .use(neis)
+  .use(notifications)
+  .listen(process.env.PORT ?? 3000);
 
 console.log(`
 🍤 Slunch-V2 backend is running at ${app.server!.url}
