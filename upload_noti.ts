a276eb53722a3385aadb1a2b294066d6d60ffe0b
ny64 <me@ny64.kr>
2025-03-18 19:57:52 +0900

    refactor uploadNotification to use editor input and handle response

--- upload_noti.ts
+++ upload_noti.ts
@@ -1,16 +1,24 @@
-export {}
+import { password, input, editor } from '@inquirer/prompts';
 
-import { password, input } from "@inquirer/prompts"
+async function uploadNotification() {
+  const response = await fetch('https://slunch-v2.ny64.kr/notifications/', {
+    method: 'POST',
+    headers: {
+      Token: await password({ message: 'token' }),
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: await input({ message: 'title' }),
+      content: (await editor({ message: 'content' })).trim(),
+      date: new Date().toISOString(),
+    }),
+  });
 
-fetch('https://slunch-v2.ny64.kr/notifications/', {
-  method: 'POST',
-  headers: {
-    Token: await password({ message: 'token' }),
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    title: await input({ message: 'title' }),
-    content: await Bun.file("notification.txt").text(),
-    date: (new Date()).toISOString()
-  })
-})
+  if (response.status !== 200) {
+    console.error(await response.json());
+  } else {
+    console.log('Success');
+  }
+}
+
+uploadNotification();
